60c2739e827f56bc5d634e0e7afb2bdeac2329af
lkstudio.dante <lkstudio.dante@gmail.com>
2024-07-23 15:38:33 +0900

    no message

--- Client/Code_DirectX_Sample/Code_DirectX_Sample/00009902000101-Code_DirectX_Sample/Classes/Global/Define/KDefine+Global.h
+++ Client/Code_DirectX_Sample/Code_DirectX_Sample/00009902000101-Code_DirectX_Sample/Classes/Global/Define/KDefine+Global.h
@@ -2,4 +2,4 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <memory>
-#include "Packages/00000302000101-Module_DirectX_Common/Client/Classes/Global/Define/KDefine+Abs.h"
+#include "Packages/00000302000101-Module_DirectX_Common/Client/Classes/Global/Define/KCDefine+Abs.h"
